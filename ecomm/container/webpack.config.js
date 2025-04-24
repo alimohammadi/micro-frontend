b5f4f218537258efeaa9a0e93ff172fbe33dc3ac
Ali Mohammadi <alimohammadi02321@gmail.com>
2025-04-24 13:58:50 +0330

    Improve/ use cart part data in container

--- ecomm/container/webpack.config.js
+++ ecomm/container/webpack.config.js
@@ -19,6 +19,7 @@ module.exports = {
       name: "container",
       remotes: {
         products: "products@http://localhost:8081/remoteEntry.js",
+        carts: "cart@http://localhost:8082/remoteEntry.js",
       },
     }),
     new HtmlWebpackPlugin({
