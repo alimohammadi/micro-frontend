02977ce34aba29060231547e958ddf30e5801624
Ali Mohammadi <alimohammadi02321@gmail.com>
2025-04-27 12:24:53 +0330

    Feature/ setup new marketing with react and add files and folders

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+// Mount function to start up the app
+const mount = (el) => {
+  ReactDom.render(<h1>Hi there! i am react</h1>, el);
+};
+// if we are in development mode and in isolation call mount imediately
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_marketing-dev-root");
+
+  if (devRoot) mount(devRoot);
+}
+
+// We are running through container and we should export the mount function
+export { mount };
